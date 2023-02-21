25e97d7ee02315a94b5c1fc027f975a0091dabe6
Giovanni De Toni <giovanni.det@gmail.com>
2023-02-21 16:20:23 +0100

    Add init to make install work.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     setup(
         name='rl_mcts',
         version='0.0.1',
-        packages=find_packages(include=["rl_mcts", "rl_mcts.*"]),
+        packages=find_packages(include=["rl_mcts", "rl_mcts.*", "rl_mcts.core.*"]),
         url='',
         license='MIT',
         author='Giovanni De Toni',
